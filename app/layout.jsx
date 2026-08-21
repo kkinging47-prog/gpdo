9806f12742a2fbf116edfed4dda794bc75f5895f
kkinging47-prog <kkinging47@gmail.com>
2026-08-21 15:34:52 +0000

    Step 8: load news and article styles

--- app/layout.jsx
+++ app/layout.jsx
@@ -6,6 +6,8 @@ import './events.css';
 import './admin-events.css';
 import './programs-public.css';
 import './programs-admin.css';
+import './news.css';
+import './admin-articles.css';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import RevealManager from '../components/RevealManager';
