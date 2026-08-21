e9170aae3fc8f148ceee1dd48cb84e3ed1f1a469
kkinging47-prog <kkinging47@gmail.com>
2026-08-21 10:06:51 +0000

    Step 5: load slideshow styles

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,7 @@
 import './globals.css';
 import './admin.css';
+import './slideshow.css';
+import './slides-admin.css';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import RevealManager from '../components/RevealManager';
